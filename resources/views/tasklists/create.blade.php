16441c2da2cde61ffb1dc05e381e0e7c62e759c1
EC2 Default User <ec2-user@ip-172-31-19-2.us-east-2.compute.internal>
2018-06-07 06:17:30 +0000

    bootstrap design

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -6,14 +6,17 @@
 
     {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
     
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+　　</div>
 
-
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
+    
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
--- resources/views/tasklists/index.blade.php
+++ resources/views/tasklists/index.blade.php
@@ -4,14 +4,26 @@
 
 <h1>Task一覧</h1>
 
-    @if (count($tasklists) > 0)
-        <ul>
-            @foreach ($tasklists as $tasklist)
-                <li>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!} : {{ $tasklist->status }} > {{ $tasklist->content }}
-</li>
-            @endforeach
-            {!! link_to_route('tasklists.create', '新規Taskの投稿') !!}
-        </ul>
+     @if (count($tasklists) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>ステータス</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasklists as $tasklist)
+                    <tr>
+                        <td>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!}</td>
+                        <td>{{ $tasklist->status }}</td>
+                        <td>{{ $tasklist->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
+    {!! link_to_route('tasklists.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
     
 @endsection
